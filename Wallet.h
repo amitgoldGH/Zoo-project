ba3c18f1b64e266732100abac73ef0bcd909f7b6
Amit Goldshtein <Amit.Goldshtein@s.afeka.ac.il>
2019-09-09 19:36:46 +0300

    I dont remember what I did

--- Wallet.h
+++ Wallet.h
@@ -8,15 +8,10 @@ class Wallet
 public:
 	void set_Amount(int sum);	//set initial amount in wallet
 	int get_Money();			//get sum in wallet
-	int pay_Money(int amount);	//decrease amount of money
-
+	int pay_Money(int amount);	//decrease specific amount of money
 
 private:
 	int money;					//current sum in wallet
-
-
-
-
 };
 
 #endif
